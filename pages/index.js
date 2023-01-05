c3c602e35bfa9e8c5b19df1c0a2a202f5d4ba583
Ismael Rumzan <ismael.rumzan@gmail.com>
2023-01-05 09:30:36 -0700

    add layout

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
 import { API } from '@stoplight/elements';
 
 export default function Home() {
   if (typeof window !== 'undefined') {
     return (
-      <div>
-        <API apiDescriptionUrl='https://openapi.vercel.sh/' />
-      </div>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <div>
+          <API apiDescriptionUrl='https://openapi.vercel.sh/' />
+        </div>
+      </Layout>
     );
   }
 }
